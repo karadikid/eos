cc4eb27907a9e099bc8764560ebcb16cecd0a4f5
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-13 11:13:47 -0400

    added in button and list component

--- eos-react/src/App.js
+++ eos-react/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { JsonRpc } from "eosjs";
+import "./App.css";
+// import axios from "axios";
+import Button from "./Components/Button";
+import List from "./Components/BlockList"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blocks: [],
+      loading: true,
+      load: false
+    };
+  }
+
+  fetchData() {
+    let url = 'https://api.eosnewyork.io';
+    this.rpc = new JsonRpc(url, { fetch });
+      this.getBlocks();
+  }
+
+  
+  render() {
+
+    return (
+      <>
+        <Button />
+        <List />
+      </>
+    );
+  }
 }
 
 export default App;
