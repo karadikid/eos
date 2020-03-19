64cd0b815a88ea118e0323182bc9383688a80210
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-03-19 10:25:43 -0400

    send updates

--- eos-react/src/App.js
+++ eos-react/src/App.js
@@ -1,34 +1,71 @@
 import React, { useState, useEffect } from "react";
 import { JsonRpc } from "eosjs";
 import "./App.css";
-// import axios from "axios";
 import Button from "./Components/Button";
 import List from "./Components/List";
+import Loader from "./Components/Loader";
 
 const App = () => {
   const [blocks, setBlocks] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  
+
+  // function reloadBlocks(e) {
+  //   e.preventDefault();
+  //   fetchData();
+  //   setBlocks(blocks);
+  // }
+
   async function fetchData() {
     let blockChainData;
-    const rpc = new JsonRpc('https://api.eosnewyork.io', { fetch });
+    let currentBlock;
+    let pastBlock;
+    const rpc = new JsonRpc("https://api.eosnewyork.io", { fetch });
     try {
       blockChainData = await rpc.get_info();
-      console.log(blockChainData)
+      console.log(blockChainData);
+      currentBlock = await rpc.get_block(
+        blockChainData.head_block_num
+      );
+      blocks.push(currentBlock);
+      console.log(blocks);
     } catch (error) {
       console.error(JSON.stringify(error));
     }
-  };
+
+    // let i = 0;
+
+    // do {
+    //   try {
+    //     pastBlock = await rpc.get_block(currentBlock.previous);
+    //     blocks.push(fetchDetails(pastBlock));
+    //   } catch (error) {}
+    // } while (i < 10);
+    // console.log(blocks);
+  }
+
+  function fetchDetails(block) {
+    let blockDetails = {
+      id: block.id,
+      timestamp: block.timestamp,
+      transactions: block.transactions.length,
+      details: JSON.stringify(block)
+    };
+    return blockDetails;
+  }
 
   useEffect(() => {
-   fetchData();
-  })
-  return (
-    <>
-      <Button />
-      <List />
-    </>
-  );
+    fetchData();
+  });
+
+  if (blocks.length < 1) {
+    return <Loader />;
+  } else {
+    return (
+      <>
+        <Button />
+        <List />
+      </>
+    );
+  }
 };
 
 export default App;
